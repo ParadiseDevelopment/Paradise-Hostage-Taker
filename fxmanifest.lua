2e5a5e82ebdb676e5a03b617890fde3b61b68064
ParadiseDevelopment <84147084+ParadiseDevelopment@users.noreply.github.com>
2021-05-13 18:12:09 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+-- Resource Metadata
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'Paradise'
+description 'TakeHostage'
+version '2.0.0'
+
+client_script "client.lua"
+server_script "server.lua"
